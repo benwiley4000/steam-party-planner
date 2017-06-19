37ec77425a198ba4086370f9ce2c589820255c2c
Ben Wiley <therealbenwiley@gmail.com>
2017-06-18 22:05:07 -0400

    Table styled.

--- public/app.js
+++ public/app.js
@@ -13,6 +13,7 @@ var app = new Vue({
       }).then(function (data) {
         this.games = data.games.map(game => {
           game.steam_id_count = game.steam_ids.length;
+          game.playtime_in_hours = (game.playtime_forever / 60).toFixed(1);
           return game;
         });
       }.bind(this));
@@ -24,5 +25,7 @@ var app = new Vue({
 // table component. This makes fetch before mount necessary.
 // https://github.com/spatie/vue-table-component/issues/13
 app.fetchGames().then(() => {
+  document.getElementById('loading-message').classList.add('hidden');
+  document.getElementById('steam-party-planner').classList.remove('hidden');
   app.$mount('#steam-party-planner');
 });
